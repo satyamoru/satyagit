9e7ec0ebb2c5cd271f60ccb658740105989856e1
satyamoru <morusatyam@gmail.com>
2022-08-03 19:12:37 +0530

    changed value

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ class Main {
   public static void main(String[] args) {
     
     System.out.println("Enter two numbers");
-    int first = 10;
+    int first = 20;
     int second = 20;
     
     System.out.println(first + " " + second);
