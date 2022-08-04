b967f9ac6e0c33fe61787231281ba9ee5fa0148c
satyamoru <morusatyam@gmail.com>
2022-08-04 17:24:01 +0530

    Added Changes

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ class Main {
     
     System.out.println("Enter two numbers");
     int first = 30;
-    int second = 20;
+    int second = 30;
     
     System.out.println(first + " " + second);
 
